cad953e8ed2b47f0c13f169915daaa4244175d01
Jas Eckard <eckard@clemson.edu>
2015-02-11 16:13:23 -0500

    Added checks if prerequisites are installed, user not running as root.

--- harbinger.sh
+++ harbinger.sh
@@ -1,5 +1,23 @@
-sudo iw dev wlan0 interface add mon0 type monitor
-sudo ifconfig mon0 up
-sudo python ./sensor.py
-sudo ifconfig mon0 down
-sudo iw dev mon0 del
+#!/bin/sh
+
+if ! IW=`/bin/which iw` ; then
+        echo "Harbinger prequires 'iw', which is not installed."
+        exit 1
+fi
+
+if ! PYTHON=`/bin/which python` ; then
+        echo "Harbinger prequires 'python', which is not installed."
+        exit 1
+fi
+
+SUDO=''
+
+if test 'root' != `whoami` ; then
+        SUDO=sudo
+fi
+
+$SUDO $IW dev wlan0 interface add mon0 type monitor
+$SUDO ifconfig mon0 up
+$SUDO $PYTHON ./sensor.py
+$SUDO ifconfig mon0 down
+$SUDO $IW dev mon0 del
